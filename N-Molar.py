5dcdb94ed540392cce7eefea5dc364d4a1003dcb
GordonQuach <qgordon8@vt.edu>
2016-11-19 03:09:54 -0500

    Add files via upload

---
+++ N-Molar.py
@@ -0,0 +1,6 @@
+# This program was written by Gordon Quach to practice more Python related skills.
+# The premise of it is to take inputs of the element symbol OR name and then print
+# out its molar mass. In addition, another option for the user is to enter the 
+# mass, and the program will give the corresponding element.
+
+
